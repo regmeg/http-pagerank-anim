6d90a82a2f4aa778687f93e01a187c90a350805d
Ara Hayrabedian <ara.hayrabedian@gmail.com>
2016-12-17 21:05:57 +0000

    Put together a reusable self-filling textbox for usage in animations

--- src/App.js
+++ src/App.js
@@ -36,6 +36,48 @@ let Animations = {
   }),
 };
 
+class SelfFulfillingTextBox extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      labeltext: props.label,
+      fullText: props.text,
+      lettersToShow: 1,
+    }
+  }
+
+  tick() {
+    this.setState((prevState) => ({
+      lettersToShow: prevState.lettersToShow + 1,
+      currentText: prevState.fullText.substring(0, prevState.lettersToShow),
+    }));
+    if (this.state.lettersToShow > this.state.fullText.length) {
+      clearInterval(this.interval)
+    }
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => this.tick(), 150);
+  }
+
+  handleSubmit(event){
+
+  }
+
+
+  render(){
+    return(
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          {this.state.labeltext}
+          <input type="text" value={this.state.currentText} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    )
+  }
+}
+
 //Define the main App component
 class App extends Component {
 
@@ -81,6 +123,8 @@ class App extends Component {
         <p className="App-intro">
           Random num is {this.state.randomNum}, checking if state is lost.
         </p>
+        <SelfFulfillingTextBox label="Search" text="something new here"/>
+        <SelfFulfillingTextBox label="Try again" text="something another here"/>
       </div>
     );
   }
