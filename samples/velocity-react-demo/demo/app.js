832a253750fe464f17336e98e490d9f1317716ed
Ara Hayrabedian <ara.hayrabedian@gmail.com>
2017-01-10 19:27:01 +0000

    remove samples

--- samples/velocity-react-demo/demo/app.js
+++
@@ -1,5 +0,0 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-var MainComponent = require('./main');
-
-ReactDOM.render(React.createElement(MainComponent), document.getElementById('app'));
